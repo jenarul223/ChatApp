b7e5bd17c1b3c76e56e4f96fdafe5fb088bb4e61
jenarul223 <jenarul223@gmail.com>
2026-08-11 16:30:57 +0530

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,27 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+// Read config from firebase-applet-config.json
+import firebaseConfig from '../firebase-applet-config.json';
+
+const app = !getApps().length
+  ? initializeApp({
+      apiKey: firebaseConfig.apiKey,
+      authDomain: firebaseConfig.authDomain,
+      projectId: firebaseConfig.projectId,
+      storageBucket: firebaseConfig.storageBucket,
+      messagingSenderId: firebaseConfig.messagingSenderId,
+      appId: firebaseConfig.appId,
+    })
+  : getApp();
+
+export const db = firebaseConfig.firestoreDatabaseId
+  ? getFirestore(app, firebaseConfig.firestoreDatabaseId)
+  : getFirestore(app);
+
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export default app;
